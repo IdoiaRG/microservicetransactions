0ad266ae4e9a1d514974f1e35773370b89a1cd2c
IdoiaRG <iarz@gft.com>
2023-05-04 13:54:42 +0000

    First

---
+++ src/main/java/com/training/microservicestransactions/MicroservicestransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.training.microservicestransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicestransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicestransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/microservicestransactions/MicroservicestransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.microservicestransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicestransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
